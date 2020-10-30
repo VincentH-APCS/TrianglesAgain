7ae34e40e34571bc49ae0d8fd950874da64f5470
VincentH-APCS <vhuang10@stuy.edu>
2020-10-29 21:40:12 -0400

    Fixed typo in getArea that was getting the wrong answer

--- Triangle.java
+++ Triangle.java
@@ -18,10 +18,10 @@ public class Triangle{
     return v1.distanceTo(v2) + v1.distanceTo(v3) + v3.distanceTo(v2);
   }
   public double getArea(){
-    double s = (v1.distanceTo(v2) + v1.distanceTo(v3) + v3.distanceTo(v2)) / 2;
+    double s =(v1.distanceTo(v2) + v1.distanceTo(v3) + v3.distanceTo(v2)) / 2;
     return  Math.sqrt((s * (s - v1.distanceTo(v2)) *
                           (s - v1.distanceTo(v3)) *
-                          (s - v2.distanceTo(v2))));
+                          (s - v2.distanceTo(v3))));
 
 }
 }
