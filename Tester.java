25089afb323464488dbe278e34b87f4d904417a4
VincentH-APCS <vhuang10@stuy.edu>
2020-10-29 21:50:35 -0400

    tested classify for an equilateral triangle

--- Tester.java
+++ Tester.java
@@ -5,6 +5,7 @@ public class Tester{
       Point P3 = new Point(-2,-1);
       Triangle Delta = new Triangle(P1, P2, P3);
       Triangle Gamma = new Triangle(0,0,4,0,4,4);
+      Triangle Omega = new Triangle(0.1,0.1,-1.9,0.1,-0.9,-1.63205);
       System.out.println(P1.distanceTo(P2));
       System.out.println(P3.distanceTo(P2));
       System.out.println(P1.distanceTo(P3));
@@ -14,6 +15,6 @@ public class Tester{
       System.out.println(Gamma.getArea());
       System.out.println(Delta.classify());
       System.out.println(Gamma.classify());
+      System.out.println(Omega.classify());
   }
-
 }
