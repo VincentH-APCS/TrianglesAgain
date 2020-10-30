429d842617d74d6adb9199f650aeeb6c3a7961e7
VincentH-APCS <vhuang10@stuy.edu>
2020-10-29 21:20:13 -0400

    tested perimeter of a triangle created with doubles as the parameters

--- Tester.java
+++ Tester.java
@@ -4,9 +4,12 @@ public class Tester{
       Point P2 = new Point(5,7);
       Point P3 = new Point(-2,-1);
       Triangle Delta = new Triangle(P1, P2, P3);
+      Triangle Gamma = new Triangle(0,0,4,0,4,4);
       System.out.println(P1.distanceTo(P2));
       System.out.println(P3.distanceTo(P2));
       System.out.println(P1.distanceTo(P3));
       System.out.println(Delta.getPerimeter());
+      System.out.println(Gamma.getPerimeter());
   }
+
 }
