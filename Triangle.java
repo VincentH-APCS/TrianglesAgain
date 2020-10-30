bebd687bb1fed126fec3f266ee3986acfc3b8d42
VincentH-APCS <vhuang10@stuy.edu>
2020-10-29 22:25:00 -0400

    Fixed part of the constructor to make toString functionable for triangles constructed from 6 doubles

--- Triangle.java
+++ Triangle.java
@@ -6,9 +6,9 @@ public class Triangle{
     v3 = c;
   }
   public Triangle(double x1, double y1, double x2, double y2,double x3, double y3){
-    Point v1 = new Point(x1,y1);
-    Point v2 = new Point(x2,y2);
-    Point v3 = new Point(x3,y3);
+    v1 = new Point(x1,y1);
+    v2 = new Point(x2,y2);
+    v3 = new Point(x3,y3);
 
   }
 
