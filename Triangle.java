ceac50be448826da7896dfc1bb5adbb7b52f5ec8
VincentH-APCS <vhuang10@stuy.edu>
2020-10-29 22:19:29 -0400

    experimenting with the toString method

--- Triangle.java
+++ Triangle.java
@@ -6,12 +6,10 @@ public class Triangle{
     v3 = c;
   }
   public Triangle(double x1, double y1, double x2, double y2,double x3, double y3){
-    Point a = new Point(x1,y1);
-    Point b = new Point(x2,y2);
-    Point c = new Point(x3,y3);
-    v1 = a;
-    v2 = b;
-    v3 = c;
+    Point v1 = new Point(x1,y1);
+    Point v2 = new Point(x2,y2);
+    Point v3 = new Point(x3,y3);
+
   }
 
   public double getPerimeter(){
@@ -37,6 +35,7 @@ public class Triangle{
     else return "scalene";
 }
   public String toString(){
-    return "v1("+v1+")v2("+v2+")v3("+v3+")";
+    return ("v1("+ v1.getX()+ "," + v1.getY() + ")v2(" + v2.getX()+ "," +
+    v2.getY() + ")v3(" + v3.getX()+ "," + v3.getY() + ")");
   }
 }
