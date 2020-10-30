efaa4a0fe15e03db4c5f3b445a26d5e09f4e8483
VincentH-APCS <vhuang10@stuy.edu>
2020-10-29 20:50:37 -0400

    Test of distanceTo

--- Tester.java
+++ Tester.java
@@ -1,7 +1,9 @@
 public class Tester{
   public static void main(String[] args){
-      Point P1 = new Point(55, 77);
-      System.out.println(P1.getX());
-      System.out.println(P1.getY());
+      Point P1 = new Point(5,7);
+      Point P2 = new Point(0,2);
+      //System.out.println(P1.getX());
+      //System.out.println(P1.getY());
+      System.out.println(P1.distanceTo(P2));
   }
 }
