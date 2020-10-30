34a5254a26b1853dd674e97fcd845b2b992956eb
VincentH-APCS <vhuang10@stuy.edu>
2020-10-29 21:15:49 -0400

    First test of the perimeter

--- Tester.java
+++ Tester.java
@@ -1,13 +1,12 @@
 public class Tester{
   public static void main(String[] args){
       Point P1 = new Point(1,1);
-      Point P2 = new Point(1,1);
-      Point P3 = new Point(0,4);
-      Point P4 = new Point(0,4);
-      //System.out.println(P1.getX());
-      //System.out.println(P1.getY());
-      //System.out.println(P1.distanceTo(P2));
-      System.out.println(P1.equals(P2));
-      System.out.println(P3.equals(P4));
+      Point P2 = new Point(5,7);
+      Point P3 = new Point(-2,-1);
+      Triangle Delta = new Triangle(P1, P2, P3);
+      System.out.println(P1.distanceTo(P2));
+      System.out.println(P3.distanceTo(P2));
+      System.out.println(P1.distanceTo(P3));
+      System.out.println(Delta.getPerimeter());
   }
 }
