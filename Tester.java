0680e673deb41dccf1984ffb0e93cc55a5b0903a
VincentH-APCS <vhuang10@stuy.edu>
2020-10-29 20:34:03 -0400

    Just a quick test to see everything's working

---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+  public static void main(String[] args){
+      Point P1 = new Point(55, 77);
+      System.out.println(P1.getX());
+      System.out.println(P1.getY());
+  }
+}
