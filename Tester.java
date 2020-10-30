787b8f2d3aeabc8895adc2be19bed9d3c53230af
VincentH-APCS <vhuang10@stuy.edu>
2020-10-29 20:57:16 -0400

    First test of equals for false

--- Tester.java
+++ Tester.java
@@ -4,6 +4,7 @@ public class Tester{
       Point P2 = new Point(-1,-1);
       //System.out.println(P1.getX());
       //System.out.println(P1.getY());
-      System.out.println(P1.distanceTo(P2));
+      //System.out.println(P1.distanceTo(P2));
+      System.out.println(P1.equals(P2));
   }
 }
